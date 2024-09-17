008a9298d2c1535d7bb22ebab0ad32fdbccc75af
victornjenga <victornjenga68@gmail.co>
2024-09-17 20:40:13 +0300

    yes

--- pages/detail/[slug].js
+++ pages/detail/[slug].js
@@ -166,7 +166,7 @@ function ProductDetails({ productDetails, products }) {
         <div className="w-full md:w-[90%] pt-32 mb-8">
           {/* Google Login Modal */}
           {isModalOpen && (
-            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="fixed inset-0 mx-3 flex items-center justify-center bg-black bg-opacity-50">
               <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
                 <h2 className="text-lg font-semibold mb-4">
                   Sign in with Google to submit your review
